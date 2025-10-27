e6196508bd8518cedfb87abc0927bb47374600ed
x10x10x <120926392+eit88888888@users.noreply.github.com>
2025-10-27 17:26:54 +0100

    Add Board

---
+++ app.js
@@ -0,0 +1,9 @@
+const gameboardObj = {
+    gameboard: [[1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9]],
+    game: {},
+    player1: {},
+    player2: {},
+}
+console.log(gameboardObj.gameboard[2][0]);
